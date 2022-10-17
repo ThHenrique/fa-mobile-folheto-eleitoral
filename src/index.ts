b12acdaf92315b4b83c9defd5333a79961aab0b0
thhenrique <henriquethiago298@gmail.com>
2022-10-17 05:12:01 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+import fs from 'fs'
+import csv from 'csv-parser'
+import { Writable, Transform } from 'stream'
+
+import { mongoClient } from './connectMongo'
+
+const readableStreamFile = fs.createReadStream('../data_csv/candidatos-2022/consulta_cand_2022_SP.csv', {
+	encoding: 'latin1',
+})
+const transformToObject = csv({ separator: ';' })
+const transformToString = new Transform({
+	objectMode: true,
+	transform(chunk, encoding, callback) {
+		callback(null, JSON.stringify(chunk))
+	},
+})
+
+const writableStreamFile = new Writable({
+	write(chunk, encoding, next) {
+		const stringifyer = chunk.toString()
+		const rowData = JSON.parse(stringifyer, (key, value: string) => {
+			if (value == '#NULO#' || value == "-1" || value == '#NE#' || value == "-3") {
+				value = null
+			}
+			if (value != null) {
+				if (value === 'S' || value === "N") {
+					return value === 'S'
+				}
+			}
+			return value
+		})
+
+		const database = mongoClient.db('santinhodb')
+		const candidatos = database.collection('candidatos2022')
+		async function handleData(rowData: any) {
+			try {
+				await candidatos.insertOne(rowData)
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		handleData(rowData)
+		// console.log('PROCESSANDO', rowData);
+		next();
+	}
+})
+
+console.log('INICIOU', Date());
+
+readableStreamFile
+	.pipe(transformToObject)
+	.pipe(transformToString)
+	.pipe(writableStreamFile)
+	.on('close', async () => {
+		console.log('FINALIZOU', Date())
+	})
+
